952143a4b00a0d99e7e79825c04afd12681e12bc
Tai Nguyen Van <buiblue123@gmail.com>
2025-12-25 16:35:40 +0700

    build and archive artifact

--- test/math.test.js
+++ test/math.test.js
@@ -4,6 +4,6 @@ test("add 2 + 3 = 5", () => {
   expect(add(2, 3)).toBe(5);
 });
 
-test("add failed 1 + 1 = 3", () => {
-  expect(add(1, 1)).toBe(3);
-});
+// test("add failed 1 + 1 = 3", () => {
+//   expect(add(1, 1)).toBe(3);
+// });
