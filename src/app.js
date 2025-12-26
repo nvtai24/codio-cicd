74d5d2f08882a4f6b6ee6e607cdc59582f78b965
Tai Nguyen Van <buiblue123@gmail.com>
2025-12-26 21:44:17 +0700

    delete old resources

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const items = ["Learn Git", "Learn CI", "Deploy App"];
-
-const list = document.getElementById("list");
-
-items.forEach((item) => {
-  const li = document.createElement("li");
-  li.textContent = item;
-  list.appendChild(li);
-});
